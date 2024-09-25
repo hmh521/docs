8e32f42e42ba3aa1b680171f695315e9a6b430c4
huminghao <565105438@qq,com>
2024-09-25 15:48:07 +0800

    feat: 隐藏私密信息

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+const headConf = require('./config/headConf.js');
+const pluginsConf = require('./config/pluginsConf.js');
+const navConf = require('./config/navConf.js');
+const sidebarConf = require('./config/sidebarConf.js');
+
+module.exports = {
+    title: "轻狂帅哥",     // 网站的标题
+    description: "轻狂帅哥的笔记",    // 网站的描述
+    head: headConf,
+    plugins: pluginsConf,
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: navConf,
+        lastUpdated: '更新时间',
+        sidebar: sidebarConf
+    },
+
+}
