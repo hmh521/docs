a30a301091c2b3e8946d910b7c8a45a891dab081
huminghao <565105438@qq,com>
2024-09-27 00:58:34 +0800

    feat: 提交图片缩放

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -39,4 +39,11 @@ module.exports = [
             id: secret.ga,
         },
     ],
+    ['@vuepress/medium-zoom',{
+        selector: 'img',
+    }],
+    ["vuepress-plugin-auto-sidebar", {
+        mode: "titlecase",
+        nav: true,
+    }]
 ]
